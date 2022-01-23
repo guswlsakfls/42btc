6cfc5a62d8443c1215b77a39626b538911c0f5cb
Hyunjin Jo <hyujo@4r10s7.42seoul.kr>
2022-01-23 17:57:31 +0900

    add push, pop

--- Push_Swap/ft_push_swap.c
+++ Push_Swap/ft_push_swap.c
@@ -6,59 +6,136 @@
 /*   By: hyujo <hyujo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:57:05 by hyujo             #+#    #+#             */
-/*   Updated: 2022/01/21 21:08:44 by hyujo            ###   ########.fr       */
+/*   Updated: 2022/01/23 17:15:39 by hyujo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
 typedef struct s_node
 {
-	int			data;
-	struct node	*prev;
-	struct node	*next;
+	int				data;
+	struct s_node	*prev;
+	struct s_node	*next;
 }	t_node;
 
-node	*ft_init(Node *ptr)
+typedef struct s_deq
 {
-	if (ptr == NULL)
+	t_node	*top;
+	t_node	*bottom;
+	int		size;
+}	t_deq;
+
+t_deq	*ft_init_deq(void)
+{
+	t_deq	*deq;
+
+	deq = malloc(sizeof(t_deq));
+	if (!deq)
+		return (NULL);
+	deq->top = NULL;
+	deq->bottom = NULL;
+	deq->size = 0;
+	return (deq);
+}
+
+t_node	*ft_init_node(int data)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->data = data;
+	node->prev = NULL;
+	node->next = NULL;
+	return (node);
+}
+
+void	ft_push_back(t_deq *deq, t_node *new_node)
+{
+	if (!deq->top)
 	{
-		ptr = malloc(sizeof(Node));
-		if (!ptr)
-			return (NULL);
-		ptr->data = 0;
-		ptr->prev = NULL;
-		ptr->next = NULL;
-		return (ptr);
+		deq->top = new_node;
+		deq->bottom = new_node;
+	}
+	else
+	{
+		new_node->prev = deq->bottom;
+		deq->bottom->next = new_node;
+		deq->bottom = new_node;
 	}
 }
 
-void	ft_node_push(t_node node, int data)
+void	ft_push_front(t_deq *deq, t_node *new_node)
 {
-	t_node	*new_node;
-
-	new_node = malloc(sizeof(node));
-	if (!node)
-		return (NULL);
-	new_node->data = data;
-	new_node->next = node->prev;
-	node->prev = NULL;
+	if (!deq->top)
+	{
+		deq->top = new_node;
+		deq->bottom = new_node;
+	}
+	else
+	{
+		new_node->next = deq->top;
+		deq->top->prev = new_node;
+		deq->top = new_node;
+	}
 }
 
-int	ft_node_pop(t_node node)
+t_node	*ft_pop_front(t_deq *deq)
 {
 	t_node	*tmp;
 
-	tmp = node;
-	node->prev = tmp->next
-	free(node)
-	return (tmp->data);
+	tmp = deq->top;
+	deq->top = deq->top->next;
+	deq->top->next->prev = NULL;
+	return (tmp);
+}
+
+t_node	*ft_pop_back(t_deq *deq)
+{
+	t_node	*tmp;
+
+	tmp = deq->bottom;
+	deq->bottom = deq->bottom->prev;
+	deq->bottom->next = NULL;
+	return (tmp);
 }
 
 int	main(int argc, char **argv)
 {
-	t_node	node;
-	
+	t_node	*node;
+	t_deq	*deq;
+	t_node	*pivot;
+	int		data;
+	int		i;
 
-	if (argv = )
-	node = ft_init(node);
+	if (argc <= 2) // 인자가 아무것도 안들어오고, 하나만 들어 왔을 때 예외처리
+		return (0);
+
+	// ft_ceate_deq
+	deq = ft_init_deq();
+	i = 1;
+	data = 0;
+	while (i < argc)
+	{
+		data = ft_atoi(argv[i]);
+		node = ft_init_node(data);
+		ft_push_back(deq, node);
+		i++;
+	}
+	deq->size = i;
+	// ft_push_swap 부분
+	pivot = deq->top;
+	printf("pivot : %d\n", deq->size / 2);
+	while (i < deq->size / 2)
+		pivot = pivot->next;
+	while (deq->top)
+	{
+		printf("%d\n", deq->top->data);
+		deq->top = deq->top->next;
+	}
 	return (0);
 }
---
+++ Push_Swap/libft/ft_strjoin.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyujo <hyujo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 11:40:33 by hyujo             #+#    #+#             */
+/*   Updated: 2021/12/04 15:51:34 by hyujo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*str;
+
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s1, s1_len + 1);
+	ft_strlcpy(str + s1_len, s2, s2_len + 1);
+	return (str);
+}
