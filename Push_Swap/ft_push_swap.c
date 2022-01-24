85d26eb3cafa6fdd397deab47705ef1fff3d7a43
guswlsakfls <guswlsakfls@gmail.com>
2022-01-24 21:32:41 +0900

    gg

--- Push_Swap/ft_push_swap.c
+++ Push_Swap/ft_push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyujo <hyujo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hyunjinjo <hyunjinjo@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:57:05 by hyujo             #+#    #+#             */
-/*   Updated: 2022/01/23 17:15:39 by hyujo            ###   ########.fr       */
+/*   Updated: 2022/01/24 21:24:29 by hyunjinjo        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ t_node	*ft_init_node(int data)
 
 void	ft_push_back(t_deq *deq, t_node *new_node)
 {
-	if (!deq->top)
+	if (!deq->bottom)
 	{
 		deq->top = new_node;
 		deq->bottom = new_node;
@@ -90,7 +90,8 @@ t_node	*ft_pop_front(t_deq *deq)
 
 	tmp = deq->top;
 	deq->top = deq->top->next;
-	deq->top->next->prev = NULL;
+	deq->top->prev = NULL;
+	tmp->next = NULL;
 	return (tmp);
 }
 
@@ -101,13 +102,119 @@ t_node	*ft_pop_back(t_deq *deq)
 	tmp = deq->bottom;
 	deq->bottom = deq->bottom->prev;
 	deq->bottom->next = NULL;
+	tmp->prev = NULL;
 	return (tmp);
+}
+
+void	ft_swap(t_node *a, t_node *b)
+{
+	int	tmp;
+
+	tmp = a->data;
+	a->data = b->data;
+	b->data = tmp;
+}
+
+void	ft_sa(t_deq *a)
+{
+	ft_swap(a->top, a->top->next);
+	ft_putstr_fd("sa\n", 1);
+}
+
+void	ft_sb(t_deq *b)
+{
+	ft_swap(b->top, b->top->next);
+	ft_putstr_fd("sb\n", 1);
+}
+
+void	ft_ss(t_deq *a, t_deq *b)
+{
+	ft_swap(a->top, a->top->next);
+	ft_swap(b->top, b->top->next);
+	ft_putstr_fd("ss\n", 1);
+}
+
+void	ft_pa(t_deq *a, t_deq *b)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_front(a);
+	ft_push_front(b, tmp);
+	ft_putstr_fd("pa\n", 1);
+}
+
+void	ft_pb(t_deq *b, t_deq *a)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_front(b);
+	ft_push_front(a, tmp);
+	ft_putstr_fd("pb\n", 1);
+}
+
+void	ft_ra(t_deq *a)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_front(a);
+	ft_push_back(a, tmp);
+	ft_putstr_fd("ra\n", 1);
+}
+
+void	ft_rb(t_deq *b)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_front(b);
+	ft_push_back(b, tmp);
+	ft_putstr_fd("rb\n", 1);
+}
+
+void	ft_rr(t_deq *a, t_deq *b)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_front(a);
+	ft_push_back(a, tmp);
+	tmp = ft_pop_front(b);
+	ft_push_back(b, tmp);
+	ft_putstr_fd("rr\n", 1);
+}
+
+void	ft_rra(t_deq *a)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_back(a);
+	ft_push_front(a, tmp);
+	ft_putstr_fd("rra\n", 1);
+}
+
+void	ft_rrb(t_deq *b)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_back(b);
+	ft_push_front(b, tmp);
+	ft_putstr_fd("rrb\n", 1);
+}
+
+void	ft_rrr(t_deq *a, t_deq *b)
+{
+	t_node	*tmp;
+
+	tmp = ft_pop_back(a);
+	ft_push_front(a, tmp);
+	tmp = ft_pop_back(b);
+	ft_push_front(b, tmp);
+	ft_putstr_fd("rrr\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
 	t_node	*node;
-	t_deq	*deq;
+	t_deq	*a;
+	t_deq	*b;
 	t_node	*pivot;
 	int		data;
 	int		i;
@@ -115,27 +222,39 @@ int	main(int argc, char **argv)
 	if (argc <= 2) // 인자가 아무것도 안들어오고, 하나만 들어 왔을 때 예외처리
 		return (0);
 
-	// ft_ceate_deq
-	deq = ft_init_deq();
+	// ft_ceate_deq(a, b)
+	a = ft_init_deq();
+	b = ft_init_deq();
 	i = 1;
 	data = 0;
 	while (i < argc)
 	{
 		data = ft_atoi(argv[i]);
 		node = ft_init_node(data);
-		ft_push_back(deq, node);
+		ft_push_back(a, node);
 		i++;
 	}
-	deq->size = i;
+	a->size = i;
 	// ft_push_swap 부분
-	pivot = deq->top;
-	printf("pivot : %d\n", deq->size / 2);
-	while (i < deq->size / 2)
+	printf("pivot : %d\n", a->size / 2);
+	while (i < a->size / 2)
 		pivot = pivot->next;
-	while (deq->top)
+
+	ft_ra(a);
+	// ft_pa(a, b);
+
+	while (a->top)
 	{
-		printf("%d\n", deq->top->data);
-		deq->top = deq->top->next;
+		printf("%d\n", a->top->data);
+		a->top = a->top->next;
 	}
+
+	printf("-\na\n\n");
+	while (b->top)
+	{
+		printf("%d\n", b->top->data);
+		b->top = b->top->next;
+	}
+	printf("-\nb\n");
 	return (0);
 }
