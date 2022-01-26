f9bc3cabe6bcedfac8e22a8ddcb492c2efdde533
Hyunjin Jo <hyujo@4r10s7.42seoul.kr>
2022-01-26 21:58:37 +0900

    good

--- Push_Swap/ft_push_swap.c
+++ Push_Swap/ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hyujo <hyujo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:57:05 by hyujo             #+#    #+#             */
-/*   Updated: 2022/01/25 21:35:00 by hyujo            ###   ########.fr       */
+/*   Updated: 2022/01/26 21:53:57 by hyujo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,8 @@ t_node	*ft_pop_front(t_deq *deq)
 	t_node	*tmp;
 
 	tmp = deq->top;
+	if (!deq->top->next) // 노드가 하나만 들어왔을 때 bottom 주소가 남아 있다.
+		deq->bottom = NULL;
 	deq->top = deq->top->next;
 	if (deq->top) // 스택에 하나의 노드값만 있게되면 top = NULL 이 되서 접근할 수 없다.
 		deq->top->prev = NULL;
@@ -104,6 +106,8 @@ t_node	*ft_pop_back(t_deq *deq)
 	t_node	*tmp;
 
 	tmp = deq->bottom;
+	if (!deq->top->next) // 노드가 하나만 들어왔을 때
+		deq->top = NULL;
 	deq->bottom = deq->bottom->prev;
 	if (deq->bottom)
 		deq->bottom->next = NULL;
@@ -122,14 +126,20 @@ void	ft_swap(t_node *a, t_node *b)
 
 void	ft_sa(t_deq *a)
 {
-	ft_swap(a->top, a->top->next);
-	ft_putstr_fd("sa\n", 1);
+	if (a->top->next) // if 문 준 이유는 하나만 들어 왔을때가 있기 때문에 그대로 내보내 주려고
+	{
+		ft_swap(a->top, a->top->next);
+		ft_putstr_fd("sa\n", 1);
+	}
 }
 
 void	ft_sb(t_deq *b)
 {
-	ft_swap(b->top, b->top->next);
-	ft_putstr_fd("sb\n", 1);
+	if (b->top->next)
+	{
+		ft_swap(b->top, b->top->next);
+		ft_putstr_fd("sb\n", 1);
+	}
 }
 
 void	ft_ss(t_deq *a, t_deq *b)
@@ -143,40 +153,48 @@ void	ft_pb(t_deq *a, t_deq *b)
 {
 	t_node	*tmp;
 
-	b->size++;
-	tmp = ft_pop_front(a);
-	ft_push_front(b, tmp);
-	ft_putstr_fd("pb\n", 1);
+	if (a->top)
+	{
+		tmp = ft_pop_front(a);
+		ft_push_front(b, tmp);
+		ft_putstr_fd("pb\n", 1);
+	}
 }
 
 void	ft_pa(t_deq *a, t_deq *b)
 {
 	t_node	*tmp;
 
-	a->size++;
-	tmp = ft_pop_front(b);
-	ft_push_front(a, tmp);
-	ft_putstr_fd("pa\n", 1);
+	if (b->top)
+	{
+		tmp = ft_pop_front(b);
+		ft_push_front(a, tmp);
+		ft_putstr_fd("pa\n", 1);
+	}
 }
 
 void	ft_ra(t_deq *a)
 {
 	t_node	*tmp;
 
-	a->size++;
-	tmp = ft_pop_front(a);
-	ft_push_back(a, tmp);
-	ft_putstr_fd("ra\n", 1);
+	if (a->top->next)
+	{
+		tmp = ft_pop_front(a);
+		ft_push_back(a, tmp);
+		ft_putstr_fd("ra\n", 1);
+	}
 }
 
 void	ft_rb(t_deq *b)
 {
 	t_node	*tmp;
 
-	b->size++;
-	tmp = ft_pop_front(b);
-	ft_push_back(b, tmp);
-	ft_putstr_fd("rb\n", 1);
+	if (b->top->next)
+	{
+		tmp = ft_pop_front(b);
+		ft_push_back(b, tmp);
+		ft_putstr_fd("rb\n", 1);
+	}
 }
 
 void	ft_rr(t_deq *a, t_deq *b)
@@ -194,18 +212,24 @@ void	ft_rra(t_deq *a)
 {
 	t_node	*tmp;
 
-	tmp = ft_pop_back(a);
-	ft_push_front(a, tmp);
-	ft_putstr_fd("rra\n", 1);
+	if (a->top->next)
+	{
+		tmp = ft_pop_back(a);
+		ft_push_front(a, tmp);
+		ft_putstr_fd("rra\n", 1);
+	}
 }
 
 void	ft_rrb(t_deq *b)
 {
 	t_node	*tmp;
 
-	tmp = ft_pop_back(b);
-	ft_push_front(b, tmp);
-	ft_putstr_fd("rrb\n", 1);
+	if (b->top->next)
+	{
+		tmp = ft_pop_back(b);
+		ft_push_front(b, tmp);
+		ft_putstr_fd("rrb\n", 1);
+	}
 }
 
 void	ft_rrr(t_deq *a, t_deq *b)
@@ -234,52 +258,122 @@ int	ft_size_deq(t_deq *deq)
 	return (i);
 }
 
+int	ft_get_pivot(t_deq *a, int size)
+{
+	t_node	*tmp;
+	int		pivot;
+	int		i;
+
+	tmp = a->top;
+	i = 1;
+	pivot = 0;
+	while (i < size)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	pivot = tmp->data;
+	return (pivot);
+}
+
+int	ft_find_pivot(t_deq *a, int size)
+{
+	int		i;
+	t_node	*tmp;
+
+	tmp = a->top;
+	i = 1;
+	while (i < size)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (tmp->data);
+}
+
 void	ft_a_to_b(t_deq *a, t_deq *b, int size)
 {
 	int	pivot;
 	int	rra;
+	int	i;
+	int	a_size;   // 구조체로 데리고 다니면 전역변수가 되서 스택을 이용할때 값이 꼬인다.
+	int	b_size;
 
 	if (size <= 1)
 		return ;
-	pivot = a->bottom->data;
-	a->size = 0;
-	b->size = 0;
-	while (--size > 0)
+	if (size == 2)
+	{
+		if (a->top->data > a->top->next->data)
+			ft_sa(a);
+		return ;
+	}
+	pivot = ft_find_pivot(a, size);
+	a_size = 0;
+	b_size = 0;
+	i = -1;
+	while (++i < size)
 	{
 		if (a->top->data > pivot)
+		{
 			ft_ra(a);
+			a_size++;
+		}
 		else
+		{
 			ft_pb(a, b);
+			b_size++;
+		}
 	}
-	rra = a->size;
-	while (--rra >= 0)
+	rra = a_size;
+	while (--rra >= 0 && a->top->next) // 노드가 2개 이상일때만 이거는 나중에 초기 예외처리에서 제외될듯
 		ft_rra(a);
-	ft_a_to_b(a, b, a->size + 1);
-	ft_b_to_a(a, b, b->size + 1);
+	ft_a_to_b(a, b, a_size);
+	ft_b_to_a(a, b, b_size);
 }
 
 void	ft_b_to_a(t_deq *a, t_deq *b, int size)
 {
 	int	pivot;
 	int	rrb;
+	int	i;
+	int	a_size;   // 구조체로 데리고 다니면 전역변수가 되서 스택을 이용할때 값이 꼬인다.
+	int	b_size;
 
 	if (size <= 1)
-		return ;
-	pivot = b->bottom->data;
-	a->size = 0;
-	b->size = 0;
-	while (--size > 0)
 	{
-		if (b->top->data > pivot)
-			ft_rb(b);
-		else
-			ft_pa(a, b);
+		ft_pa(a, b);
+		return ;
 	}
-	rrb = b->size;
-	while (--rrb >= 0)
+	if (size == 2)
+	{
+		if (b->top->data < b->top->next->data)
+			ft_sb(b);
+		ft_pa(a, b);
+		ft_pa(a, b);
+		return ;
+	}
+	pivot = ft_find_pivot(b, size);
+	a_size = 0;
+	b_size = 0;
+	i = -1;
+	while (++i < size)
+	{
+		if (b->top->data < pivot)
+		{
+			ft_rb(b);
+			b_size++;
+		}
+		else
+		{
+			ft_pa(a, b);
+			a_size++;
+		}
+	}
+	rrb = b_size;
+	while (--rrb >= 0 && b->top->next)
 		ft_rrb(b);
-	ft_a_to_b(a, b, a->size + 1);
-	ft_b_to_a(a, b, b->size + 1);
+	ft_a_to_b(a, b, a_size);
+	ft_b_to_a(a, b, b_size);
 }
 
 int	main(int argc, char **argv)
