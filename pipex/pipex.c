e752d37c27fab472c1094e63fac1e439465045b9
Hyunjin Jo <hyujo@4r10s7.42seoul.kr>
2022-02-15 19:51:04 +0900

    almost

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,16 +6,11 @@
 /*   By: hyujo <hyujo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 18:00:06 by hyujo             #+#    #+#             */
-/*   Updated: 2022/02/14 19:55:02 by hyujo            ###   ########.fr       */
+/*   Updated: 2022/02/15 19:46:48 by hyujo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pipex.h"
-
-// int	ft_parse_cmd(char **argv, char **envp)
-// {
-
-// }
 
 void	ft_print_error(int num)
 {
@@ -27,10 +22,12 @@ void	ft_print_error(int num)
 		ft_putstr_fd("infile error\n", 2);
 	else if (num == 3)
 		ft_putstr_fd("access error\n", 2);
+	else if (num == 4)
+		ft_putstr_fd("execve error\n", 2);
 	exit(1);
 }
 
-void	ft_free_cmds(char **cmds)
+void	ft_free(char **cmds)
 {
 	int	i;
 
@@ -43,6 +40,35 @@ void	ft_free_cmds(char **cmds)
 	free(cmds);
 }
 
+char	*ft_get_path(char *cmd, char **envp)
+{
+	int		i;
+	char	**paths;
+	char	*path_list;
+	char	*path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	paths = ft_split(envp[i], ':');
+	i = -1;
+	while (paths[++i])
+	{
+		path_list = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(path_list, cmd);
+		if (access(path, F_OK) == 0)
+		{
+			ft_free(paths);
+			free(path_list);
+			return (path);
+		}
+		free(path);
+	}
+	free(path_list);
+	ft_free(paths);
+	return (0);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	int		i;
@@ -50,6 +76,7 @@ int	main(int ac, char **av, char **envp)
 	int		file_fd[2];
 	char	**cmds;
 	pid_t	pid1;
+	char	*path;
 
 	i = 0;
 	if (ac < 5)
@@ -59,37 +86,34 @@ int	main(int ac, char **av, char **envp)
 	pid1 = fork();
 	if (pid1 < 0)
 		ft_print_error(1);
-	// 부모 프로세스
 	if (pid1 > 0)
 	{
 		waitpid(pid1, NULL, 0);
 		file_fd[1] = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		printf("부모\n");
-		cmds = ft_split(av[3], ' ');
-		dup2(file_fd[1], 1); // 1 은 파일에 쓴다.
-		dup2(pipe_fd[WRITE], WRITE);
+		dup2(file_fd[1], STDOUT_FILENO);
+		dup2(pipe_fd[WRITE], STDIN_FILENO);
 		close(pipe_fd[READ]);
-		execve("usr/bin/wc", cmds, envp);
+		cmds = ft_split(av[3], ' ');
+		path = ft_get_path(cmds[0], envp);
+		if (path == 0)
+			ft_print_error(3);
+		if (execve(path, cmds, envp) < 0)
+			ft_print_error(4);
 	}
-	// 자식 프로세스
 	else if (pid1 == 0)
 	{
-		printf("자식\n");
-		if (access(av[1], F_OK))
-			ft_print_error(3);
 		file_fd[0] = open(av[1], O_RDONLY);
 		if (file_fd[0] < 0)
 			ft_print_error(2);
-		dup2(file_fd[0], 0); // 0 은 파일 안에 내용을 읽어온다.
-		dup2(pipe_fd[READ], READ); // 자식은 파일을 읽어와 커맨드로 데이터를 파이프에 쓴다.
+		dup2(file_fd[0], STDIN_FILENO);
+		dup2(pipe_fd[READ], STDOUT_FILENO);
 		close(pipe_fd[WRITE]);
 		cmds = ft_split(av[2], ' ');
-		execve("/bin/ls", cmds, envp);
-		ft_free_cmds(cmds);
+		path = ft_get_path(cmds[0], envp);
+		if (path == 0)
+			ft_print_error(3);
+		if (execve(path, cmds, envp) < 0)
+			ft_print_error(4);
 	}
-
-	// ---------------printf------------------------------------------------------
-	printf("__________envp__________\n");
-	printf("%s\n", envp[3]);
 	return (0);
 }
